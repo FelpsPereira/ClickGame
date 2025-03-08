24220f54680c469234ab85dffa50a086df0e4ba3
FelipePereira392 <felipepereiraandrade392@gmail.com>
2025-03-08 12:51:42 -0300

    Básico finalizado!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let value = 0, upgradeValue = 10, multiplierValue = 1
 
 const vars = {
-    clickButton: document.querySelector('#button'),
+    clickButton: document.querySelector('#clickButton'),
     clicks: document.querySelector('#Clicks'),
     upgradeButton: document.querySelector('#upgradeButton'),
     multiplier: document.querySelector('#Multiplier'),
@@ -11,8 +11,20 @@ const vars = {
 
 
 vars.clickButton.addEventListener('click', ()=>{
-    vars.upgradeButton.style.display = 'block'
-    vars.clicks.textContent = `| Clicks: ${value = value + 1 * multiplierValue} |`
+    const divMore = document.createElement('div')
+    divMore.id = 'moreDiv'
+    divMore.textContent = `${(1 * multiplierValue).toFixed(1)}+`
+    value += 1 * multiplierValue
+    vars.clicks.textContent = `| Clicks: ${value.toFixed(0)} |`
+
+    document.body.appendChild(divMore)
+    setTimeout(() =>{
+        divMore.remove()
+    }, 500)
+
+    if (value == 10){
+        vars.upgradeButton.style.display = 'block'
+    }
 })
 
 
@@ -20,16 +32,16 @@ vars.upgradeButton.addEventListener('click', ()=>{
     if (value < upgradeValue){
         vars.clicks.textContent = `You can't buy the upgrade!`
         setTimeout(() =>{
-            vars.clicks.textContent = `| Clicks: ${value} |`
+            vars.clicks.textContent = `| Clicks: ${value.toFixed(0)} |`
         }, 1500)
     } else {
         value -= upgradeValue
-        upgradeValue *= 2
-        multiplierValue += 1
+        upgradeValue *= 3
+        multiplierValue *= 1.2
 
-        vars.clicks.textContent = `| Clicks: ${value} |`
-        vars.upgradeButton.textContent = `Upgrade Cost: ${upgradeValue}`
-        vars.multiplier.textContent = `Multiplier: ${multiplierValue}x |`
+        vars.clicks.textContent = `| Clicks: ${value.toFixed(0)} |`
+        vars.upgradeButton.textContent = `Upgrade Cost: ${upgradeValue.toFixed(0)}`
+        vars.multiplier.textContent = `Multiplier: ${multiplierValue.toFixed(1)}x |`
     }
     
 })
