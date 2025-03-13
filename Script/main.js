09090eead2b90df21aad9af0d72fd5f1c45cb69f
FelipePereira392 <felipepereiraandrade392@gmail.com>
2025-03-13 09:44:18 -0300

    Vars

--- Script/main.js
+++ Script/main.js
@@ -1,47 +1,41 @@
-let value = 0, upgradeValue = 10, multiplierValue = 1, mults = 0,isGolden = false, ownedGoldenClick = false, canBuyGoldenClick = true, clicksPerSecond = 0, points = 0;
+let value = 0, 
+upgradeValue = 10, 
+multiplierValue = 1, 
+mults = 0,
+isGolden = false, 
+ownedGoldenClick = false, 
+canBuyGoldenClick = true, 
+clicksPerSecond = 0, 
+points = 0;
 
-
-const vars = {
-    clickButton: document.querySelector('#click-button'),
-    clicks: document.querySelector('#counter-clicks'),
-    upgradeButton: document.querySelector('#upgrade-button'),
-    multiplier: document.querySelector('#counter-multiplier'),
-    counter: document.querySelector('#counter'),
-    storeButton: document.querySelector('#store-button'),
-    store: document.querySelector('#Store'),
-    GoldenClick: document.querySelector('#golden-click'),
-    GoldenClickButton: document.querySelector('#purchase-golden-click-button'),
-}
 
 vars.clickButton.addEventListener('click', ()=>{
     vars.counter.style.display = 'flex'
     const divMore = document.createElement('div')
     divMore.id = 'moreDiv'
     if (isGolden == true){
-        points += 5 * multiplierValue
         divMore.textContent = `${5 * multiplierValue}+`
-        value += 5 * multiplierValue
+        points,value += 5 * multiplierValue
         vars.clicks.textContent = `Clicks: ${value}`
         vars.clickButton.style = 'animation-name:none;'
         isGolden = false
-    } else {
+    } else{
         points += 1 * multiplierValue
         divMore.textContent = `${1 * multiplierValue}+`
         value += 1 * multiplierValue
         vars.clicks.textContent = `Clicks: ${value}`
     }
-    vars.clickButton.appendChild(divMore)
+
+    vars.counter.appendChild(divMore)
     divMore.style = 'animation-name:more;'
     setTimeout(() =>{
         divMore.remove()
-    }, 1000)
-
+    }, 600)
     if (value == 10){
         vars.upgradeButton.style = 'display:block; animation-name:surgimento;'
         document.body.style = 'justify-content: space-between;'
         vars.counter.style = 'display:flex;position: initial;'
     }
-
     if (ownedGoldenClick == true){
         const nA = Math.floor(Math.random() * 11)
         if (nA == 1) {
@@ -51,15 +45,14 @@ vars.clickButton.addEventListener('click', ()=>{
     }
 })
 
-
 vars.upgradeButton.addEventListener('click', ()=>{
     if (value < upgradeValue){
         vars.clicks.textContent = `You can't buy the upgrade!`
         vars.upgradeButton.style.backgroundColor = 'rgba(214, 30, 17, 0.521)'
         setTimeout(() =>{
             vars.clicks.textContent = `Clicks: ${value.toFixed(0)}`
-            vars.upgradeButton.style.backgroundColor = 'rgb(9, 107, 163)'
-        }, 600)
+            vars.upgradeButton.style.backgroundColor = 'rgb(93, 114, 126)'
+        }, 500)
 
     } else {
         vars.storeButton.style = 'display:block; animation-name:surgimento;'
@@ -67,32 +60,26 @@ vars.upgradeButton.addEventListener('click', ()=>{
         upgradeValue *= 5
         multiplierValue *= 2
         mults++
-
         vars.clicks.textContent = `Clicks: ${value.toFixed(0)}`
-
-
         vars.upgradeButton.style.backgroundColor = 'rgba(17, 214, 66, 0.521)'
         setTimeout(() =>{
-            vars.upgradeButton.style.backgroundColor = 'rgb(9, 107, 163)'
-        }, 600)
-
-
+            vars.upgradeButton.style.backgroundColor = 'rgb(93, 114, 126)'
+        }, 500)
         vars.upgradeButton.textContent = `Upgrade Cost: ${upgradeValue.toFixed(0)}`
-
     }
-    
 })
 
 vars.storeButton.addEventListener('click', ()=>{
     if (vars.store.style.display == 'flex') {
         vars.store.style = 'display:flex; animation-name:fadeOutStore;'
+        vars.storeButton.style = 'display:block;background-color: rgba(53, 202, 53, 0.589); box-shadow: 0px 0px 10px rgba(18, 156, 48, 0.603);'
         setTimeout(() =>{
             vars.store.style = 'display:none;'
-        }, )
+        }, 300)
     }else {
         vars.store.style = 'display:flex; animation-name:fadeInStore;'
+        vars.storeButton.style = 'display:block; background-color: rgba(223, 100, 100, 0.589); box-shadow: 0px 0px 10px rgba(255, 0, 0, 0.603);'
     }
-    
 })
 
 vars.GoldenClickButton.addEventListener('click', ()=>{
@@ -112,10 +99,10 @@ vars.GoldenClickButton.addEventListener('click', ()=>{
     }
 })
 
-
 setInterval(()=>{
     clicksPerSecond = points
     points = 0
-
     vars.multiplier.textContent = `Clicks /s: ${clicksPerSecond}`
 }, 1000)
+
+import {vars} from './vars.js'
---
+++ Script/vars.js
@@ -0,0 +1,11 @@
+export const vars = {
+    clickButton: document.querySelector('#click-button'),
+    clicks: document.querySelector('#counter-clicks'),
+    upgradeButton: document.querySelector('#upgrade-button'),
+    multiplier: document.querySelector('#counter-multiplier'),
+    counter: document.querySelector('#counter'),
+    storeButton: document.querySelector('#store-button'),
+    store: document.querySelector('#Store'),
+    GoldenClick: document.querySelector('#golden-click'),
+    GoldenClickButton: document.querySelector('#purchase-golden-click-button'),
+}
